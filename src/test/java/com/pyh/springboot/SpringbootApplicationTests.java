c2d03ec97dc69d2cf721b1bd1d328d5a176c7ffa
peng_yaohui <248463200@qq.com>
2021-01-24 18:45:27 +0800

    springboot mvc

--- src/test/java/com/pyh/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/pyh/springboot/SpringbootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.pyh.springboot;
 
+import com.pyh.springboot.sp02_jsr303.Jsr303;
 import com.pyh.springboot.sp01_di.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,17 @@ class SpringbootApplicationTests {
 
 	@Autowired
 	private User user;
+	@Autowired
+	private Jsr303 jsr303;
 	@Test
 	void contextLoads() {
 		System.out.println(user);
+	}
+
+
+	@Test
+	void contextLoads1() {
+		System.out.println(jsr303);
 	}
 
 }
